b8d3bfd78d4e7eef8e06ecb707d55fe6bd9f9b42
Mohamed Ali <mohamedalif163@gmail.com>
2025-02-11 22:33:41 +0200

    First Task : Mission

---
+++ ppc_mission/ppc_mission/mission_client.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import sys
+import rclpy 
+from rclpy.node import Node
+from functools import partial
+from std_msgs.msg import String
+from ppc_interfaces.srv import MissionSRV
+
+
+
+
+class MissionClientNode(Node):
+    def __init__(self):
+        super().__init__("mission_client")
+        self.mission_client = self.create_client(MissionSRV,"start_mission")
+        while not self.mission_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Service not available ,waiting again .....")
+        
+
+    def send_mission(self):
+        self.requst = MissionSRV.Request()
+        self.requst.mission_name = sys.argv[1]
+
+        if self.requst.mission_name == "GoTo":
+            self.requst.x_pose = float(sys.argv[2])
+            self.requst.y_pose = float(sys.argv[3])
+
+        future = self.mission_client.call_async(self.requst)
+        future.add_done_callback(partial(self.callback_status))
+
+    def callback_status(self, future):
+        try:
+            response = future.result()  
+            if response.accepted:  
+                self.get_logger().info("Service accepted the mission.")
+            else:
+                self.get_logger().info("Service rejected the mission.")
+        except Exception as e:
+            self.get_logger().error(f"Service call failed: {e}")
+
+def main(argv=None):
+    rclpy.init()
+    node = MissionClientNode()
+    node.send_mission()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ ppc_mission/ppc_mission/mission_server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import rclpy 
+from rclpy.node import Node
+from ppc_interfaces.srv import MissionSRV
+from ppc_interfaces.msg import MissionMSG
+
+
+class MissionServerNode(Node):
+    def __init__(self):
+        super().__init__("mission_service")
+        self.publish_to_mission = self.create_publisher(MissionMSG,"mission",10)
+        self.mission_servic = self.create_service(MissionSRV,"start_mission",self.mission_service_callback)
+        
+
+    def mission_service_callback(self,requst,response):
+        mission_msg = MissionMSG()
+        mission_msg.mission_name= requst.mission_name
+        if mission_msg.mission_name == "GoTo":
+            mission_msg.x_pose = requst.x_pose
+            mission_msg.y_pose = requst.y_pose
+            self.get_logger().info(f"Go to ({mission_msg.x_pose},{mission_msg.y_pose}).")
+            response.accepted = True
+            self.publish_to_mission.publish(mission_msg)
+        elif mission_msg.mission_name == "Stop":
+            self.get_logger().info("Go to idle mode")
+            response.accepted = True
+            self.publish_to_mission.publish(mission_msg)
+        else :
+            self.get_logger().info("Not accepted")
+            response.accepted = False
+        
+        return response
+
+
+def main(argv=None):
+    rclpy.init()
+    node = MissionServerNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+
+if __name__ == "__main__":
+    main()
